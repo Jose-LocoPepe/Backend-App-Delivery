0c339d54347a1329f7ead325b4db7c07f033dcc8
Nagorik11 <fernando.valenzuela01@alumnos.ucn.cl>
2024-04-18 21:37:23 -0400

    new file:   src/models/category.js 	new file:   src/models/product.js

---
+++ src/models/category.js
@@ -0,0 +1,34 @@
+const { DataTypes, Model } = require('sequelize');
+const dbConnect = require('../database/connection');
+
+class Category extends Model {
+    static id;
+    static name;
+    static description;
+    static image;
+}
+
+Category.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize: dbConnect,
+    modelName: 'Category'
+});
+
+module.exports = Category;
---
+++ src/models/product.js
@@ -0,0 +1,44 @@
+const { DataTypes, Model } = require('sequelize');
+const dbConnect = require('../database/connection');
+
+class Product extends Model {
+    static id;
+    static name;
+    static description;
+    static price;
+    static image;
+    static categoryId;
+}
+
+Product.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    categoryId: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize: dbConnect,
+    modelName: 'Product'
+});
+
+module.exports = Product;
