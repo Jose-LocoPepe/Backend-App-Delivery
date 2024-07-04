eccbd7914543e3bfa11ba35303d11da2a48fbae6
Ertinho <105566974+Ertinho@users.noreply.github.com>
2024-07-03 21:32:20 -0400

    feat(orderRoute): add route fields valitadion

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -8,15 +8,40 @@ const { validateJWT } = require("../middlewares/validate-jwt");
 
 const router = Router();
 
-router.patch("/:orderId/dispatch", [validateJWT, validateAdmin], OrderController.dispatchOrder);
-router.patch("/:orderId/deliver", [validateJWT,], OrderController.deliverOrder); // Falta el middleware de Repartidor
-router.patch("/:orderId/startDelivery", [validateJWT,], OrderController.deliverOrder); // Falta el middleware de Repartidor
+router.patch("/:orderId/dispatch", [validateJWT, validateAdmin,
+    check("orderId", "El id de la orden es obligatorio").not().isEmpty(),
+    check("deliveryUserId", "El id del repartidor es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.dispatchOrder);
+router.patch("/:orderId/deliver", [validateJWT,
+    check("orderId", "El id de la orden es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.deliverOrder); // Falta el middleware de Repartidor
+router.patch("/:orderId/startDelivery", [validateJWT,
+    check("orderId", "El id de la orden es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.startDelivery); // Falta el middleware de Repartidor
 
-router.get("/:userId/pending", validateJWT, OrderController.getPendingPurchaseOrders);
-router.get("/:userId/dispatched", validateJWT, OrderController.getDispatchedPurchaseOrders);
-router.get("/:userId/delivered", validateJWT, OrderController.getDeliveredPurchaseOrders);
-router.get("/:userId/on-the-way", validateJWT, OrderController.getOnTheWayPurchaseOrders);
+router.get("/:userId/pending", [validateJWT,
+    check("userId", "El id del usuario es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.getPendingPurchaseOrders);
+router.get("/:userId/dispatched", [validateJWT,
+    check("userId", "El id del usuario es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.getDispatchedPurchaseOrders);
+router.get("/:userId/delivered", [validateJWT,
+    check("userId", "El id del usuario es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.getDeliveredPurchaseOrders);
+router.get("/:userId/on-the-way", [validateJWT,
+    check("userId", "El id del usuario es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.getOnTheWayPurchaseOrders);
 
-router.get("/:orderId/products", validateJWT, OrderController.getProductsByOrderId);
+router.get("/:orderId/products", [validateJWT,
+    check("orderId", "El id de la orden es obligatorio").not().isEmpty(),
+    validateFields
+], OrderController.getProductsByOrderId);
 
 module.exports = router;
